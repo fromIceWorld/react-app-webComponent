ad3248a9c30d1705a34d1c92b7e0d0de836c77b3
Cui Bingbing <752856108@qq.com>
2023-11-16 14:39:15 +0800

    feat:取消react挂载;修改全局样式

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import { BackgroundImage } from './components/background-image/background-image'
 
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
