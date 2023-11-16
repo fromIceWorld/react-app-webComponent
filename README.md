c47d990c8f2b53660364ca2f9aa0c0b9b0046e3c
Cui Bingbing <752856108@qq.com>
2023-11-16 14:02:48 +0800

    feat:调整目录

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-component
-将react组件打包成web-component，发布到work-bench。
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'build'), // 输出路径
-        filename: 'bundle.js',
-    },
-};
